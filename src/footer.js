8a7be665c32c07be20fef25c386d9d55f1545f86
Jorge Abdallah <jorgeea1205@gmail.com>
2021-02-11 18:05:18 -0400

    Added social icons

---
+++ src/footer.js
@@ -0,0 +1,47 @@
+
+function renderFooter (){
+    const socialNet = [
+        {
+            'name': 'instagram',
+            'icon': ['fab','fa-instagram'],
+            'link': 'www.google.com'
+        },
+        {
+            'name': 'whatsapp',
+            'icon': ['fab','fa-whatsapp'],
+            'link': 'https://www.google.com'
+        }
+    ];
+
+    let footer = document.createElement('footer');
+    footer.classList.add('footer-container');
+
+    let title = document.createElement('span');
+    title.innerText = 'Síguenos:';
+
+    let socialContainer = document.createElement('div');
+    socialContainer.classList.add('social');
+
+    socialNet.forEach(item=>{
+        let itemCont = document.createElement('div');
+        itemCont.classList.add('social-item',item.name);
+        let link = document.createElement('a');
+        let icon = document.createElement('i');
+
+        link.href = item.link;
+
+        item.icon.forEach(iconClass=>{
+            icon.classList.add(iconClass)
+        })
+
+        link.append(icon);
+        itemCont.append(link);
+        socialContainer.append(itemCont);
+    })
+
+    footer.append(title,socialContainer);
+
+    return footer;
+}
+
+export {renderFooter as default}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import header from './header'
+import footer from './footer';
 
 const content = document.getElementById('content');
 
 
-content.append(header())
+content.append(header(),footer())
 
 
